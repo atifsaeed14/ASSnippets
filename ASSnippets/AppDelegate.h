7270c7c2001ce0c200376be433a46a57e35ddef0
Atif Saeed <netsolace@emax-mujtaba.netsolaceisb.com>
2015-02-24 10:08:41 +0500

    Initial Commit

---
+++ ASSnippets/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASSnippets
+//
+//  Created by Atif Saeed on 2/24/15.
+//  Copyright (c) 2015 atti14. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASSnippets/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASSnippets
+//
+//  Created by Atif Saeed on 2/24/15.
+//  Copyright (c) 2015 atti14. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
